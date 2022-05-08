7b96fccdcd359cee98a118520af7a1a6a7b8eab0
Vincent Jiang <vincentjiang2003@gmail.com>
2022-05-08 01:03:46 -0400

    fixed children error; login still not running correctly

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,22 +1,31 @@
 import styles from '../styles/Login.module.css'
 import React from 'react'
-import { useAuth } from "../components/auth/login"
+import { useAuth } from "../components/auth/AuthUserProvider"
 import { signInWithGoogle } from '../util/firebase'
 
 const Login = () => {
     const { user } = useAuth()
     return (
+    
     <div className={styles.main}>
         <div className={styles.login}>
+            {/**
             {user ? (
                 <Login />
             ) : (
+            <button
+                //onClick={signInWithGoogle}
+            >
+                Login
+            </button>
+            )} 
+
+            Login */}
             <button
                 onClick={signInWithGoogle}
             >
                 Login
             </button>
-            )}
         </div>
     </div>
     )
