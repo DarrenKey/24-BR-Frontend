793ec45e530c7088608fa9de4a13fa77495e4cb7
Vincent Jiang <vincentjiang2003@gmail.com>
2022-05-08 00:05:52 -0400

    pushed initial code

--- components/auth/login.tsx
+++ components/auth/login.tsx
@@ -1,4 +1,22 @@
-import { db } from "../../util/firebase"
-import { collection, onSnapshot, query } from "firebase/firestore"
+import { User } from "firebase/auth"
+import { createContext, useContext, FC, Children } from "react"
+import { WrappedComponentProps } from "react-with-firebase-auth"
+import { createComponentWithAuth } from "../../util/firebase"
 
-const usersColl = collection(db, "users")
+type AuthData = Omit<WrappedComponentProps, "user"> & {
+  user?: User | null
+}
+
+const AuthUserContext = createContext<AuthData | undefined>(undefined)
+
+const AuthUserProvider: FC<WrappedComponentProps> = ({children, ...auth}) => (
+  <AuthUserContext.Provider value={auth}>{children}</AuthUserContext.Provider>
+)
+
+export default createComponentWithAuth(AuthUserProvider)
+
+export const useAuth = () => {
+  const context = useContext(AuthUserContext)
+  if (!context) throw new Error("AuthUserContext has no value")
+  return context
+}
--- pages/login.tsx
+++ pages/login.tsx
@@ -1,11 +1,22 @@
 import styles from '../styles/Login.module.css'
 import React from 'react'
+import { useAuth } from "../components/auth/login"
+import { signInWithGoogle } from '../util/firebase'
 
 const Login = () => {
+    const { user } = useAuth()
     return (
     <div className={styles.main}>
         <div className={styles.login}>
-            Login
+            {user ? (
+                <Login />
+            ) : (
+            <button
+                onClick={signInWithGoogle}
+            >
+                Login
+            </button>
+            )}
         </div>
     </div>
     )
